08cb3b1b838c550845fc28a9574dea0f46f2dadd
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-01-09 11:30:40 -0300

    Arquivo PHP para teste de funcionalidade

---
+++ src/car-details.php
@@ -0,0 +1,322 @@
+<html lang="pt-br">
+	<head>
+		<!-- Google -->
+		<meta name="description" content="Site com catálogo para compra de veículos">
+		<meta name="author" content="">
+
+		<!-- Meta tags Obrigatórias -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- CSS -->
+		<link href="../../vendor/bootstrap-4/bootstrap.min.css" rel="stylesheet">
+		<link href="../../css/default.css" rel="stylesheet">
+    <link href="../../css/car_details.css" rel="stylesheet">
+		<link href="../../css/style.css" rel="stylesheet">
+
+		<!-- Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+
+		<title>CLA - Caminhões e Veículos</title>
+		<link rel="shortcut icon" href="../../img/favicon.png" type="image/x-icon" />
+  </head>
+  <body>
+    <!-- Conteúdo -->
+    <div class="content">
+      <!-- NavBar -->
+      <nav class="navbar navbar-expand-lg navbar-dark">
+        <i style="font-size: 30px; color: #fff; margin-right: 10px;" class="fas fa-car"></i>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+						<li class="nav-item active">
+							<a class="nav-link">Início<span class="sr-only"></span></a>
+						</li>
+            <li class="nav-item dropdown active">
+							<a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown"
+								aria-haspopup="true" aria-expanded="false">
+								Marcas
+							</a>
+							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<a class="dropdown-item">Volkswagen</a>
+								<a class="dropdown-item">Mercedes</a>
+								<a class="dropdown-item">Volvo</a>
+							</div>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link">Sobre Nós<span class="sr-only"></span></a>
+						</li>
+					</ul>
+        </div>
+      </nav>
+
+      <!-- /NavBar -->
+
+      <!-- Container -->
+      <div class="container">
+        <!-- BarraPesquisa -->
+        <center>
+          <div id="divBarraPesquisa" class="form-group mb-4">
+            <form>
+              <input id="barraPesquisa" type="text" class="form-control form-control-underlined border-danger" placeholder="Procure por um veículo" autocomplete="off">
+            </form>
+          </div>
+        </center>
+      </div>
+      <!-- /BarraPesquisa -->
+      <div class="container">
+
+        <a class="title_car"> Golf MK7 GTI Automático DSG</a>
+        <br>
+        <a class="subt_car">Volkswagen - Ano: 2017 - Preço: R$124.055,00</a>
+
+        <!-- Carousel Mostruário -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel" style="border: solid 2px;">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img src="../../img/test1.jpg" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+              <img src="../../img/test2.jpg" class="d-block w-100">
+            </div>
+            <div class="carousel-item">
+              <img src="../../img/test3.jpg" class="d-block w-100">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        <br>
+        <!-- /Carousel Mostruário -->
+
+        <!-- Description -->
+        <div class="container">
+
+          <a class="desc_title"> Geral </a>
+          <hr class="hr_maior">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Procedência: </a>
+              <a class="desc_item_2"> Importado </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Garantia: </a>
+              <a class="desc_item_2"> 3 anos </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Configuração: </a>
+              <a class="desc_item_2"> Hatch </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Porte: </a>
+              <a class="desc_item_2"> Médio </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Ocupantes: </a>
+              <a class="desc_item_2"> 5 </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Portas: </a>
+              <a class="desc_item_2"> 4 </a>
+            </div>
+          </div>
+
+          <br>
+
+          <a class="desc_title"> Motor </a>
+          <hr class="hr_maior">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Instalação: </a>
+              <a class="desc_item_2"> Dianteiro </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Aspiração: </a>
+              <a class="desc_item_2"> Turbocompressor </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Disposição: </a>
+              <a class="desc_item_2"> Transversal </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Alimentação: </a>
+              <a class="desc_item_2"> Injeção direta e indireta multiponto </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Cilindros: </a>
+              <a class="desc_item_2"> 4 em linha </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Comando de válvulas: </a>
+              <a class="desc_item_2"> Duplo no cabeçote, corrente </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Variação do Comando: </a>
+              <a class="desc_item_2"> Adimissão e escape </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Válvulas por Cilindro: </a>
+              <a class="desc_item_2"> 4 </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Diâmetro dos Cilindros </a>
+              <a class="desc_item_2"> 82,5 mm </a>
+            </div>
+
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Taxa de Compressão: </a>
+              <a class="desc_item_2"> 9,6:1 </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Curso dos Pistões: </a>
+              <a class="desc_item_2"> 92,8 mm </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Cilindrada: </a>
+              <a class="desc_item_2"> 1984 cm³ </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Potência: </a>
+              <a class="desc_item_2"> 220 </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Combustível: </a>
+              <a class="desc_item_2"> Gasolina </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Torque: </a>
+              <a class="desc_item_2"> 35,7 </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Peso/Potência: </a>
+              <a class="desc_item_2"> 5,99 </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Torque Específico: </a>
+              <a class="desc_item_2"> 17,99 kgfm/litro </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Peso/Torque: </a>
+              <a class="desc_item_2"> 36,89 </a>
+            </div>
+          </div>
+
+          <hr class="hr_menor">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Potência Específica: </a>
+              <a class="desc_item_2"> 110,89 cv/litro </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Código do Motor: </a>
+              <a class="desc_item_2"> EA888 </a>
+            </div>
+          </div>
+
+          <br>
+
+          <a class="desc_title"> Transmissão </a>
+          <hr class="hr_maior">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Tração: </a>
+              <a class="desc_item_2"> Dianteira </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Câmbio: </a>
+              <a class="desc_item_2"> Automatizado </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Embreagem: </a>
+              <a class="desc_item_2"> Dupla, banhada a óleo </a>
+            </div>
+          </div>
+
+          <br>
+
+          <a class="desc_title"> Suspensão </a>
+          <hr class="hr_maior">
+
+          <div class="row">
+            <div class="col">
+              <a class="desc_item"> Dianteira: </a>
+              <a class="desc_item_2"> Independente, McPherson </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Traseira: </a>
+              <a class="desc_item_2"> Independente, multibraço </a>
+            </div>
+            <div class="col">
+              <a class="desc_item"> Embreagem: </a>
+              <a class="desc_item_2"> Dupla, banhada a óleo </a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+      <!-- Rodapé -->
+      <br>
+      <footer id="rodape">
+        <div class="footer-copyright text-center py-3" >© 2019 Copyright</div>
+      </footer>
+
+    </div>
+      <!-- Fim conteúdo -->
+
+    <!-- Scripts Obrigatórios -->
+    <!-- Jquery JS -->
+    <script src="../../vendor/jquery/3.4.1.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="../../vendor/bootstrap-4/bootstrap.min.js"></script>
+    <!-- FontAwesome JS -->
+    <script src="https://kit.fontawesome.com/04be2c50c3.js" crossorigin="anonymous"></script>
+  </body>
+</html>
