2f05b197b17f8ffaef6db08e14e4329a09ee7564
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-01-22 19:33:08 -0300

    Criando mensagem de 'Nenhum veículo encontrado'

--- js/busca-veiculos.js
+++ js/busca-veiculos.js
@@ -1,12 +1,16 @@
+var alertaResultados = document.querySelector(".sem-resultados");
+alertaResultados.classList.add("invisivel");
+
 var barraPesquisa = document.getElementById("barraPesquisa");
 var veiculo = document.querySelectorAll(".veiculo");
 
 barraPesquisa.addEventListener("input", buscaVeiculos);
 
 function buscaVeiculos()
-{
-    for (var i = 0; i < veiculo.length; i++){
+{   
+    let resultados = 0;
 
+    for (var i = 0; i < veiculo.length; i++){
         nomeVeiculo = veiculo[i].querySelector(".card-title").textContent;
         expressaoRegular = new RegExp(barraPesquisa.value, "i");
 
@@ -14,7 +18,16 @@ function buscaVeiculos()
             veiculo[i].classList.add("invisivel");
         } else {
             veiculo[i].classList.remove("invisivel");
+            resultados++;
         }
+    }
+
+    // Mostrando alerta de sem resultados
+    if (resultados == 0 && barraPesquisa.value != "")
+    {
+        alertaResultados.classList.remove("invisivel");
+    } else{
+        alertaResultados.classList.add("invisivel");
     }
 
     // Mostrando todos os veiculos
