433075cf30ce6f93181b0a70d743d12c85b2e1e5
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-03-19 13:52:45 -0300

    Sistema para mostrar ao usuario onde está o erro no cadastro

--- js/form-validacao.js
+++ js/form-validacao.js
@@ -4,10 +4,12 @@ function validaCampo() {
     
     for(let i = 0; i <= (campo.length - 1); i++)
     {
-        if (campo[i].value == "")
-        {
+        if (campo[i].value == ""){
             alert("O campo '" + campo[i].name + "' não pode ficar em branco!");
+            campo[i].classList.add("erro-form");
             return false;
+        } else {
+            campo[i].classList.remove("erro-form");
         }
     }
 }
