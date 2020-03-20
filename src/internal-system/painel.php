f08eae75deb9d8fb9007e955e879fef1a00da0f0
Everton Dias <31146819+Lemon42@users.noreply.github.com>
2020-03-20 09:46:38 -0300

    Adicionando Gráfico no Painel

--- src/internal-system/painel.php
+++ src/internal-system/painel.php
@@ -1,5 +1,12 @@
 <?php
 require('../server-functions/autentica-login.php');
+header('Content-Type: text/html; charset=utf-8');
+include("../server-functions/connect.php");
+
+$carro = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM veiculos WHERE tipo = 'Carro'"));
+$moto = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM veiculos WHERE tipo = 'Moto'"));
+$caminhao = mysql_fetch_array(mysql_query("SELECT COUNT(*) FROM veiculos WHERE tipo = 'Caminhao'"));
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -10,7 +17,6 @@ require('../server-functions/autentica-login.php');
 
 		<!-- CSS -->
 		<link href="../../vendor/bootstrap-4/bootstrap.min.css" rel="stylesheet">
-		<link href="../../css/login-style.css" rel="stylesheet">
 
 		<!-- Fonts -->
 		<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
@@ -19,16 +25,30 @@ require('../server-functions/autentica-login.php');
 		<link rel="stylesheet" href="../../vendor/menu/css/demo.css">
     	<link rel="stylesheet" href="../../vendor/menu/css/pushy.css">
 
-		<title>Login Funcionários</title>
+		<title>Painel dos Funcionários</title>
 		<link rel="shortcut icon" href="../../img/engrenagem.png" type="image/x-icon" />
 
 		<style>
 			body{font-family: 'Roboto', sans-serif;}
+
+			#chartdiv {
+				width: 100%;
+				height: 500px;
+			}
+
+			.invisivel{
+  				display: none;
+			}
 		</style>
 	</head>
 	<body>
-
 		<?php include("../modules/system-menu.php"); ?>
+
+		<div id="chartdiv"></div>
+		
+		<div id="carros" class="invisivel"><?=$carro[0]?></div>
+		<div id="motos" class="invisivel"><?=$moto[0]?></div>
+		<div id="caminhoes" class="invisivel"><?=$caminhao[0]?></div>
 
 		<!-- Scripts Obrigatórios -->
 		<!-- Jquery JS -->
@@ -39,5 +59,57 @@ require('../server-functions/autentica-login.php');
         <script src="../../vendor/menu/js/pushy.min.js"></script>
         <!-- FontAwesome JS -->
         <script src="https://kit.fontawesome.com/04be2c50c3.js" crossorigin="anonymous"></script>
+
+		<!-- Gráfico (amcharts) -->
+		<script src="https://www.amcharts.com/lib/4/core.js"></script>
+		<script src="https://www.amcharts.com/lib/4/charts.js"></script>
+		<script src="https://www.amcharts.com/lib/4/themes/dataviz.js"></script>
+		<script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+
+		<script>
+			am4core.ready(function () {
+
+				// Pegando informações
+				let carros = document.querySelector("#carros").innerHTML;
+				let motos = document.querySelector("#motos").innerHTML;
+				let caminhoes = document.querySelector("#caminhoes").innerHTML;
+
+				// iniciando Themes
+				am4core.useTheme(am4themes_dataviz);
+				am4core.useTheme(am4themes_animated);
+
+				// Criando instancia do chart
+				var chart = am4core.create("chartdiv", am4charts.PieChart);
+
+				// Add informações
+				chart.data = [{
+					"country": "Carros",
+					"litres": carros
+				}, {
+					"country": "Motos",
+					"litres": motos
+				}, {
+					"country": "Caminhões",
+					"litres": caminhoes
+				}];
+
+				// Set inner radius
+				chart.innerRadius = am4core.percent(50);
+
+				// Add e Configurando Series
+				var pieSeries = chart.series.push(new am4charts.PieSeries());
+				pieSeries.dataFields.value = "litres";
+				pieSeries.dataFields.category = "country";
+				pieSeries.slices.template.stroke = am4core.color("#fff");
+				pieSeries.slices.template.strokeWidth = 2;
+				pieSeries.slices.template.strokeOpacity = 1;
+
+				// Animação Inicial
+				pieSeries.hiddenState.properties.opacity = 1;
+				pieSeries.hiddenState.properties.endAngle = -90;
+				pieSeries.hiddenState.properties.startAngle = -90;
+
+			});
+		</script>
 	</body>
 </html>
